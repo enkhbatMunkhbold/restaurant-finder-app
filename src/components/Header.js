3d9c145d959e40662846ea3e90eb321025543b22
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2024-06-10 21:52:16 -0700

    Initiate app and create Header component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../style/header.css"
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1>RESTAURANT FINDER
+        <img src='../restaurant.png' alt='restaurant logo...'/>
+        
+      </h1>
+    </div>
+  )
+}
+
+export default Header
